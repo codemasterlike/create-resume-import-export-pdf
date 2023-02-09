a05f966e988480df21cd149b6950710612035196
codemaster <trinhbaloi@gmail.com>
2023-02-09 18:41:46 +0800

    Update Layout.tsx

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -10,20 +10,13 @@ const Layout: React.FC<{ fluid?: boolean, children: JSX.Element | JSX.Element[] 
           <Link href="/">
             <a className="brand"><img src={Logo.src} alt="CR" /></a>
           </Link>
-          <div className="nav__items">
-            <a href="https://github.com/sschandi/create-resume/" rel="noreferrer" className="nav-link">Repository</a>
-            <a href="https://github.com/sschandi/create-resume/issues" rel="noreferrer" className="nav-link">Issues</a>
-            <a href="mailto:createresume@chandi.ca" className="nav-link">Contact</a>
-            <Link href="/app">
-              <button className="btn btn-primary btn-link">App</button>
-            </Link>
-          </div>
+          
         </nav>
       </div>
       <main className={fluid ? 'container container__fluid' : 'container'}>{children}</main>
       <footer id="footer">
         <div className="container">
-          <p>Create Resume by <a href="https://github.com/sschandi" rel="noreferrer" className="link">Petrotopic.com</a></p>
+          <p>Create Resume by <a href="https://petrotopic.com" rel="noreferrer" className="link">Petrotopic.com</a></p>
         </div>
       </footer>
     </>
